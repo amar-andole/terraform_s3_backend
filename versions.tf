eeec67c1971f6fce5328332c79e26995c3d4bda4
amar-andole <amarandole122@gmail.com>
2024-06-09 18:45:25 +0530

    terraform_s3_backend_files

--- versions.tf
+++ versions.tf
@@ -3,6 +3,6 @@ terraform {
     bucket = "my-s3-remote-backend-123456"
     key = "state/terraform.tfstate"
     region = "ap-south-1"
-    dynamodynamodb_table = "backend-locking" 
+    dynamodb_table = "backend-locking" 
   }
 }
