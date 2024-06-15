23a6d209374d599778d14199bfde3131d4012456
amar-andole <159524627+amar-andole@users.noreply.github.com>
2024-06-15 18:52:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
-# module "root_create_s3_backend" {
-#      source = "./modules/s3_backend"
-#      s3_bucket_name = var.root_s3_bucket_name
-#      sse_algorithm = var.root_sse_algorithm
+ module "root_create_s3_backend" {
+      source = "./modules/s3_backend"
+      s3_bucket_name = var.root_s3_bucket_name
+      sse_algorithm = var.root_sse_algorithm
   
-# }
-# module "root_create_dynamodb_locking" {
-#     source = "./modules/dynamodb"
-#     dynamodb_name = var.root_dynamodb_name
-#     hash_key = var.root_hash_key
+}
+ module "root_create_dynamodb_locking" {
+     source = "./modules/dynamodb"
+     dynamodb_name = var.root_dynamodb_name
+     hash_key = var.root_hash_key
   
-# }
+}
 
-# module "create_ec2" {
-#     source = "./modules/ec2_create"
-#     instance_type = "t2.micro"
-#     key_name = "from-terra"
-#     key_path = "./keys/ec2.pem"
-#     sg_name = "terraform_sg_ssh"
-# }
+ module "create_ec2" {
+     source = "./modules/ec2_create"
+     instance_type = "t2.micro"
+     key_name = "from-terra"
+     key_path = "./keys/ec2.pem"
+     sg_name = "terraform_sg_ssh"
+}
